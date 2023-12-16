eeeecd05e6819433828bd06ecf154db943b35128
Jaa-ack <jasonman92123@gmail.com>
2023-12-16 23:09:48 +0800

    寫完博客來的搜尋內容

--- DataStructureFinal/src/Detail.java
+++ DataStructureFinal/src/Detail.java
@@ -4,14 +4,17 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Detail {
 	private HashMap<String, String> searchingResult;
-	private String author, recommender, catalogue;
+	private String author, catalogue;
+	private ArrayList<String> recommender;
 	
 	public Detail(HashMap<String, String> searchingResult) {
 		this.searchingResult = searchingResult;
+		recommender = new ArrayList<String>();
 	}
 	
 	public void getDetail(KeywordList keywords) {
@@ -20,7 +23,33 @@ public class Detail {
 			try {
 				String content = fetchContent(url);
 				if (title.contains("博客來")) {
+					// 取得作者
+					String temp = content.substring(content.indexOf("作者"));
+					author = temp.substring(0, temp.indexOf("追蹤作者"));
 					
+					// 取得推薦人
+					temp = content.substring(content.indexOf("名人推薦"), content.indexOf("好評推薦"));
+					while (temp.contains("、")) {
+						recommender.add(temp.substring(temp.indexOf(0, temp.indexOf("、"))));
+						temp.substring(temp.indexOf("、"));
+					}
+					temp = content.substring(content.indexOf("好評推薦「"), content.indexOf("作者介紹"));
+					while (temp.contains("——")) {
+						if (temp.contains("「")) {
+							recommender.add(temp.substring(temp.indexOf("——"), temp.indexOf("「")));
+							temp = temp.substring(temp.indexOf("「"));
+						}else {
+							recommender.add(temp.substring(temp.indexOf("——")));
+							break;
+						}
+					}
+					
+					// 取得目錄
+					catalogue = content.substring(content.indexOf("目錄"), content.indexOf("序"));
+					
+					// 取得簡介並評分
+					temp = content.substring(content.indexOf("內容簡介"), content.indexOf("推薦"));
+					int score = new Score(temp).score(keywords);
 				}else if (title.contains("Readmoo")) {
 					
 				}else if (title.contains("誠品")) {
@@ -51,62 +80,4 @@ public class Detail {
 
 		return retVal;
 	}
-	
-	public int BoyerMoore(String T, String P){
-    	int i = T.length();
-    	int j = P.length();
-    	int times = 0;    
-    	
-    	for (int a = j; a < i; a++) {
-    		Boolean hasP = true;
-    		for (int x = j - 1; x >= 0; x--) {
-    			String badCharacter = Character.toString(T.charAt(a + x - j));
-    			String character = Character.toString(P.charAt(x));
-        		
-    			if (!(badCharacter.equals(character))) {
-        			hasP = false;
-        			String Preverse = new StringBuffer(P).reverse().toString();
-        			
-        			// bad-character shift rule
-        			int bcShift = 0;
-        			if (Preverse.indexOf(badCharacter, j - x - 1) == -1) {
-        				bcShift = x;
-        			}else {
-        				bcShift = Preverse.indexOf(badCharacter, j - x - 1) - (j - x - 1) - 1;
-        			}
-        			
-        			// good-suffix shift rule
-        			String goodSuffix = P.substring(x+1);
-        			int gsShift = 0;
-        			if (goodSuffix.length() > 0) {
-        				String test = new StringBuffer(goodSuffix).reverse().toString();
-            			
-        				for (int n = 0; n < goodSuffix.length(); n++) {
-            				test = new StringBuffer(goodSuffix.substring(n)).reverse().toString();
-            				if (Preverse.indexOf(test, j - x - 1) != -1) {
-            					gsShift = Preverse.indexOf(test, j - x - 1);
-            				}
-            			}
-            				
-            			if (gsShift == 0)
-            				gsShift = j - 1;
-            			
-        			}
-        			
-        			if (bcShift > gsShift) {
-        				a += bcShift;
-        			}else {
-        				a += gsShift;
-        			}
-        			break;
-        		}
-        	}
-    		
-    		if (hasP) {
-    			times++;
-    		}
-    	}
-
-        return times;
-    }
 }
